3f50510f5e5e472a43fffe3a7f0eac3e316b7c44
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-10-30 21:58:32 +0200

    craeted about file and added description

---
+++ vite-project/src/Components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h2>About </h2>
+            <p> Frontend developer with a passion for coding and creating responsive and functional web applications.
+               My journey in web development began when I enrolled in coding bootcamps to learn the necessary skills to become a developer.
+               Since then, I have continued to learn and grow as a developer, and I am constantly exploring new technologies and techniques to improve my skills.
+               When I'm not coding, I enjoy  going for jogs and exploring music.  </p>
+        </div>
+    );
+}
+
+export default About;
