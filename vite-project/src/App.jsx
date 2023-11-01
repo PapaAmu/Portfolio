f2193bb50fafdeba64c2c3577800589ab4bf5604
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-01 23:52:17 +0200

    encapsulated  elements into browser router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,34 @@
+import React from 'react';
 import './App.css'
 import './index.css'
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Navbar from './Components/Navbar'
 import Home from './Components/Home';
 import About from './Components/About';
 import TechnicalSkills from './Components/TechnicalSkills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import ContactForm from './Components/ContactForm';
-import Navbar from './Components/Navbar';
 
-
-
-
-function App() {
-  return (
-    <div className='App'>
-       <BrowserRouter> 
-    <Routes>
-      <Navbar />
-      <Route path='/' element={<Navbar />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/technicalskills' element={<TechnicalSkills />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/contactform' element={<ContactForm />} />
-    </Routes>
-     </BrowserRouter>
-    < Footer/>
-    </div>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route path="/navbar" element={<Navbar />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/technicalskills" element={<TechnicalSkills />} />
+      <Route path="/project" element={<Projects />} />
+      <Route path="/contactForm" element={<ContactForm />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
   )
+)
+
+function App({routes}) {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
 }
 
-export default App
+export default App;
