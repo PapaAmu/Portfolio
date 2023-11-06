e27a026a63905f85d32b6b7fee2fa54c1a8e2121
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-06 23:56:58 +0200

    used tailwind material to make the form responsive

--- vite-project/src/Components/ContactForm.jsx
+++ vite-project/src/Components/ContactForm.jsx
@@ -1,31 +1,56 @@
-import React, { useState } from 'react';
-
-function ContactForm() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
-
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+  } from "@material-tailwind/react";
+  import { Textarea } from "@material-tailwind/react";
+   
+  export  default function ContactForm() {
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
-                Email:
-                <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </label>
-            <label>
-                Message:
-                <textarea name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-            </label>
-            <button type="submit">Submit</button>
+        <div className="container-fluid mb-4 mt-12">
+       <Card color="transparent" shadow={false} className="items-center mt-6 mb-4">
+        <Typography variant="h4" color="blue-gray">
+         Get In  Touch
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 items-center">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography vvariant="static" color="blue-gray" className="-mb-3">
+              Full Name
+            </Typography>
+            <Input
+              variant="static"
+              size="lg"
+              placeholder="Name here..."
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              variant="static"
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <div className="flex w-96 flex-col gap-6">
+            <Textarea variant="static"  placeholder="Anything in mind ..." />
+            </div>
+        
+          </div>
+        
+          <Button className="mt-6" fullWidth>
+            Submit
+          </Button>
         </form>
+      </Card>
+      </div>
     );
-}
-
-export default ContactForm;
+  }
