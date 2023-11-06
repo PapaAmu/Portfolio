f5386d1059ff376ba5e8e2f784f7624d9a2342b4
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-06 18:36:37 +0200

    added screen breakpoints

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+  "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
+  container: {
+    center: true,
+  },
   plugins: [],
-}
+});
 
