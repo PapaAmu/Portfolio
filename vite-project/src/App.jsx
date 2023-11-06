34c14c1830b857b364155bc3b5600357a975e0f3
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-06 23:54:13 +0200

    Renamed navbar component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css'
 import './index.css'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Navbar from './Components/Navbar'
+import Navbar from './Components/Nav'
 import Home from './Components/Home';
 import About from './Components/About';
 import TechnicalSkills from './Components/TechnicalSkills';
@@ -12,11 +12,11 @@ import ContactForm from './Components/ContactForm';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-      <Route path="/navbar" element={<Navbar />} />
+    <Route path="/" element={<Navbar />}>
+      <Route path="/Home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/technicalskills" element={<TechnicalSkills />} />
-      <Route path="/project" element={<Projects />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="/contactForm" element={<ContactForm />} />
       <Route path="/contact" element={<Contact />} />
     </Route>
@@ -27,6 +27,11 @@ function App({routes}) {
   return (
     <>
       <RouterProvider router={router}/>
+      <Home />
+      <About />
+      <TechnicalSkills />
+      <Projects />
+      <Contact />
     </>
   );
 }
