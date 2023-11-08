d7881c88b470bc4da954fdd53fa7887354ff5ea2
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-08 13:30:24 +0200

    added margin left and right to the paragraph

--- vite-project/src/Components/About.jsx
+++ vite-project/src/Components/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Typography} from "@material-tailwind/react";
+
 
 function About() {
     return (
         <div>
-            <h2 className='about  mb-2'>About </h2>
-            <p> Frontend developer with a passion for coding and creating responsive and functional web applications.
+            <Typography variant="h4" color="blue-gray" className=" mb-6 mt-6 text-center font-serif">
+            About 
+          </Typography>
+            <p className="mb-8 text-center ml-6 mr-5 whitespace-normal "> Frontend developer with a passion for coding and creating responsive and functional web applications.
                My journey in web development began when I enrolled in coding bootcamps to learn the necessary skills to become a developer.
                Since then, I have continued to learn and grow as a developer, and I am constantly exploring new technologies and techniques to improve my skills.
                When I'm not coding, I enjoy  going for jogs and exploring music.  </p>
