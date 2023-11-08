cb952c193585ab5908d8a7033418ffa26d5d89d0
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-08 13:32:15 +0200

    used head-shot and made the page responsive

--- vite-project/src/Components/ContactForm.jsx
+++ vite-project/src/Components/ContactForm.jsx
@@ -9,7 +9,7 @@ import {
    
   export  default function ContactForm() {
     return (
-        <div className="container-fluid mb-4 mt-12">
+        <div className="mx-auto my-8 mb-4">
        <Card color="transparent" shadow={false} className="items-center mt-6 mb-4">
         <Typography variant="h4" color="blue-gray">
          Get In  Touch
@@ -28,8 +28,8 @@ import {
                 className: "before:content-none after:content-none",
               }}
             />
-            <Typography variant="h6" color="blue-gray" className="-mb-3">
-              Your Email
+            <Typography vvariant="static" color="blue-gray" className="-mb-3">
+              Your eMail 
             </Typography>
             <Input
               variant="static"
@@ -41,6 +41,9 @@ import {
               }}
             />
             <div className="flex w-96 flex-col gap-6">
+            <Typography vvariant="static" color="blue-gray" className="-mb-3">
+              Message
+            </Typography>
             <Textarea variant="static"  placeholder="Anything in mind ..." />
             </div>
         
--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -1,43 +1,29 @@
 import React from 'react';
+import { Button } from "@material-tailwind/react";
 
 function Home() {
     return (
-        <div className="container-fluid text-center start-here">
-            <div className="row align-items-center">
-                <div className="col-sm-4 mt-0 first-head" style={{ marginTop: "-50px" }}>
-                    <h1 id="my-header">Hi, I'm </h1>
-                    <h3 className="my-name">Jenise Lukhele </h3>
-                    <h2 id="my-header">Frontend Developer</h2>
-                    <button type="button" className="btn btn-primary mt-4 " id="my-header">
-                        <i className="fa-sharp fa-regular fa-circle-down" style={{ color: "#fff" }}></i>
-                        <a href="Hlobisile Lukhele resume (10).pdf" download="HlobisileLukhele Resume" target="_blank">
-                            Resume
-                        </a>
-                    </button>
-                    <button type="button" className="btn btn-secondary mt-4  " id="my-header">
-                        Get in Touch
-                    </button>
-                </div>
-                <div className="col-sm-7 offset-sm-1 ">
-                    <img src="images/Illustration.PNG" className="img-fluid illustration" alt="illustration computer with code" />
-                </div>
-            </div>
-
-            {/* Social Links */}
-            <div className="container-fluid text-center mt-4 social-links">
-                <a href="https://www.linkedin.com/in/hlobisile-lukhele-8812b9183/" target="_blank">
-                    <i className="fa-brands brands-one  fa-linkedin-in fa-xl" style={{ color: "#d76f37" }}></i>
-                </a>
-                <a href="https://github.com/HlobisileLukhele" target="_blank">
-                    <i className="fa-brands  fa-brands-one fa-github fa-xl" style={{ color: "#d76f37" }}></i>
-                </a>
-                <a href="https://www.n.thwala03@gmail.com/" target="_blank">
-                    <i className="fa-brands brands-one fa-solid fa-at fa-xl" style={{ color: "#d76f37" }}></i>
-                </a>
-            </div>
-        </div>
+     <div class="max-w-md mx-auto overflow-hidden md:max-w-2xl justify-center text-lg gap-2 mt-3" >
+     <div class="md:flex">
+     <div class="p-8">
+      <div class="uppercase tracking-wide text-2xl text-black font-semibold text-center leading-relaxed">Hi, I am</div>
+       <p class="block mt-1 text-4xl leading-tight text-blachover:underline text-indigo-500 font-semibold mb-2 tracking-wide text-center">Jenise Lukhele</p>
+       <p class="text-slate-200 text-black text-2xl font-semibold text-center mb-3">Fronted Developer</p>
+       <div className="sm:flex text-center ml-5">
+       <Button variant="outlined" size="sm" className='mt-6 me-2 items-center'>
+        <a href="Hlobisile Lukhele resume (10).pdf" target="_blank" download="HlobisileLukhele Resume" ></a>
+           Resume
+        </Button>
+        <Button variant="filled" size="sm" className='mt-6 me-2'>
+           Reach Out
+        </Button>
+       </div>
+    </div>
+    <div class="flex mx-auto sm:justify-items-center float-center!">
+      <img class="md:h-60 md:w-70 mb-4, lg:h-60 md:w-60 mb-4, sm:h-60 w-60 text-center ml-20"src="public/images/head-shot image.png" alt="Head-shot image" />
+    </div>
+  </div>
+</div>
     );
 }
-
-
  export default Home;
