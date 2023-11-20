ae786f5e421092ab7580a8c6bf80ef7d560ecb53
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-20 22:42:37 +0200

    imported link

--- vite-project/src/Components/About.jsx
+++ vite-project/src/Components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Typography} from "@material-tailwind/react";
+import {Link} from 'react-router-dom';
 
 
 function About() {
     return (
         <div>
+        <Link to="/about">
             <Typography variant="h4" color="blue-gray" className=" mb-6 mt-6 text-center font-serif">
             About 
           </Typography>
@@ -12,6 +14,7 @@ function About() {
                My journey in web development began when I enrolled in coding bootcamps to learn the necessary skills to become a developer.
                Since then, I have continued to learn and grow as a developer, and I am constantly exploring new technologies and techniques to improve my skills.
                When I'm not coding, I enjoy  going for jogs and exploring music.  </p>
+        </Link>
         </div>
     );
 }
