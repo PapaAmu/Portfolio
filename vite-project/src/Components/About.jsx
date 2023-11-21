53931cb912152d388ab729fb7d7633ec73c3a253
Hlobisile Lukhele <n.thwala03@gmail.com>
2023-11-21 21:50:29 +0200

    added an id

--- vite-project/src/Components/About.jsx
+++ vite-project/src/Components/About.jsx
@@ -5,8 +5,7 @@ import {Link} from 'react-router-dom';
 
 function About() {
     return (
-        <div>
-        <Link to="/about">
+        <div id="about">
             <Typography variant="h4" color="blue-gray" className=" mb-6 mt-6 text-center font-serif">
             About 
           </Typography>
@@ -14,7 +13,6 @@ function About() {
                My journey in web development began when I enrolled in coding bootcamps to learn the necessary skills to become a developer.
                Since then, I have continued to learn and grow as a developer, and I am constantly exploring new technologies and techniques to improve my skills.
                When I'm not coding, I enjoy  going for jogs and exploring music.  </p>
-        </Link>
         </div>
     );
 }
