8f7c105a42cbedae29abb0cde2cd337bb8317e7d
HlobisileLukhele <n.thwala03@gmail.com>
2024-01-04 13:38:08 -0800

    updated footer year to 2024

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full bg-white p-4">
       <hr className="my-4 border-blue-gray-50" />
       <Typography color="blue-gray" className="text-center font-normal">
-        &copy; 2023 Hlobisile Lukhele
+        &copy; 2024 Hlobisile Lukhele
       </Typography>
     </footer>
   );
