56b45d9da322d40ed6289133e9351fd402654092
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-29 23:12:33 +0200

    removed unused imports and border outline for the skills icons

--- vite-project/src/Components/TechnicalSkills.jsx
+++ vite-project/src/Components/TechnicalSkills.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import {Typography} from "@material-tailwind/react";
-import { motion } from 'framer-motion';
-import { Card } from 'react-bootstrap';
 
 const TechnicalSkills = () => {
     return (
@@ -14,34 +11,24 @@ const TechnicalSkills = () => {
          
           <div className="grid grid-rows-3 gap-2 ">
           <div className="grid  grid-cols-3 gap-3 text-center items-center">
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/HTML5.png" className="h-5 w-6 inline-block"/>
                 <p className="inline-block"> HTML</p>
-                </div>
 
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/CSS3.png" className="h-5 w-6 inline-block"/>
                 <p className="inline-block">CSS</p>
-                </div>
 
-                <div className="inline-block outline outline-blue-500 rounded-md JS">
                 <img src="public/svg/Javascript_logo.png" className="h-4 w-5 inline-block Javascript_logo items-center"/>
                 <p className="inline-block javascript">JAVASCRIPT</p>
-                </div>
           </div>
           </div>
           
           <div className="grid grid-rows-3 gap-2 ">
           <div className="grid grid-cols-3 gap-3 text-center items-center">
-                <div className="inline-block outline outline-blue-500  rounded-md">
                 <img src="public/svg/Jquery.png" className="h-6 w-6 inline-block" />
                 <p className="inline-block">JQUERY</p>
-                </div>
 
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Redux.png" className="h-6 w-6 inline-block" />
                 <p className="inline-block">REDUX</p>
-                </div>
           </div>
           </div>
           </div>
@@ -51,48 +38,35 @@ const TechnicalSkills = () => {
 
           <div className="grid grid-rows-1 gap-4 ">
           <div className="grid  gap-4 grid-cols-2 text-center">
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Git.png" className="h-7 w-7 inline-block" />
                 <p className=" inline-block">GIT</p>
-                </div>
 
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Github.png" className="h-7 w-7 inline-block" />
                 <p className="inline-block">GITHUB</p>
-                </div>
           </div>
           </div>
 
           <div className="grid grid-rows-2 gap-4">
           <div className="grid  grid-cols-2 gap-4 text-center items-center">
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Boostrap.png" className="h-5 w-5 inline-block"/>
                 <p className="inline-block">BOOSTRAP</p>
-                </div>
 
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Tailwind.png"className="h-6 w-6 inline-block"/>
-                <p className="inline-block">TAILWIND</p>
-                </div>
-                
+                <p className="inline-block">TAILWIND</p>                
           </div>
           </div>
 
           <div className="grid grid-rows-1 gap-4">
           <div className="grid gap-2 grid-cols-2">
-                <div className="outline outline-blue-500  inline-block rounded-md text-center items-center">
                 <img src="public/svg/React.png" className="h-6 w-6 inline-block"/>
                 <p className="inline-block">REACT</p>
-                </div>
           </div>
           </div>
         
           <div className="grid grid-rows-2  gap-4 ">
           <div className="grid  grid-cols-2 gap-4 text-center items-center">
-                <div className="inline-block outline outline-blue-500 rounded-md">
                 <img src="public/svg/Mysql.png" className="h-7 w-7 inline-block"/>
                 <p className="inline-block">MYSQL</p>
-                </div>
           </div>
           </div>
 
