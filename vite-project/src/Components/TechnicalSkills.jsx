5522b179a58ed34a5b363f0b16f534f1dd7a36d9
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-01-29 23:56:10 +0200

    aligned the skills icons horizontal

--- vite-project/src/Components/TechnicalSkills.jsx
+++ vite-project/src/Components/TechnicalSkills.jsx
@@ -3,40 +3,24 @@ import {Typography} from "@material-tailwind/react";
 const TechnicalSkills = () => {
     return (
         <>
-        <Typography variant="h4" color="blue-gray" className="text-center font-serif mb-4 mt-8">
+        <Typography variant="h4" color="blue-gray" className="text-center font-serif mb-4 mt-4">
            Technical Skills
           </Typography>
          
-                <img src="public/svg/HTML5.png" className="h-5 w-6 inline-block"/>
-                <p className="inline-block"> HTML</p>
+         <div className="columns-12 mt-4">
+               <img src="public/svg/HTML5.png" className="h-12 w-12  mb-4"/>
+                <img src="public/svg/CSS3.png" className="h-12 w-12 mt-4 mb-4"/>
+                <img src="public/svg/Javascript_logo.png" className="h-12 w-12 mt-4 mb-4"/>
+                <img src="public/svg/Jquery.png" className="h-12 w-12 mt-4 mb-4" />
+                <img src="public/svg/Boostrap.png" className="h-12 w-12 mt-4 mb-4"/>
+                <img src="public/svg/Figma.png" className="h-12 w-14"/>
+                <img src="public/svg/Git.png" className="h-13 w-12" />
+                <img src="public/svg/Github.png" className="h-12 w-12" />
+                <img src="public/svg/Tailwind.png"className="h-16 w-16"/>
+                <img src="public/svg/React.png" className="h-12 w-14"/>
+                <img src="public/svg/Rest API.png" className="h-12 w-14"/>
 
-                <img src="public/svg/CSS3.png" className="h-5 w-6 inline-block"/>
-                <p className="inline-block">CSS</p>
-
-                <img src="public/svg/Javascript_logo.png" className="h-4 w-5 inline-block Javascript_logo items-center"/>
-                <p className="inline-block javascript">JAVASCRIPT</p>
-          
-                <img src="public/svg/Jquery.png" className="h-6 w-6 inline-block" />
-                <p className="inline-block">JQUERY</p>
-
-                <img src="public/svg/Redux.png" className="h-6 w-6 inline-block" />
-                <p className="inline-block">REDUX</p>
-
-                <img src="public/svg/Git.png" className="h-7 w-7 inline-block" />
-                <p className=" inline-block">GIT</p>
-
-                <img src="public/svg/Github.png" className="h-7 w-7 inline-block" />
-                <p className="inline-block">GITHUB</p>
-
-                <img src="public/svg/Boostrap.png" className="h-5 w-5 inline-block"/>
-                <p className="inline-block">BOOSTRAP</p>
-
-                <img src="public/svg/Tailwind.png"className="h-6 w-6 inline-block"/>
-                <p className="inline-block">TAILWIND</p>                
-
-                <img src="public/svg/React.png" className="h-6 w-6 inline-block"/>
-                <p className="inline-block">REACT</p>
-        
+         </div>
         </>
     );
 };
