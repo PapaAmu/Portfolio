18e36c136bb5120557f20d56135536cf83c42c71
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-02 15:32:20 +0200

    wrapped the skills icons in a border

--- vite-project/src/Components/TechnicalSkills.jsx
+++ vite-project/src/Components/TechnicalSkills.jsx
@@ -7,19 +7,37 @@ const TechnicalSkills = () => {
            Technical Skills
           </Typography>
          
-         <div className="columns-12 mt-4">
-               <img src="public/svg/HTML5.png" className="h-12 w-12  mb-4"/>
-                <img src="public/svg/CSS3.png" className="h-12 w-12 mt-4 mb-4"/>
-                <img src="public/svg/Javascript_logo.png" className="h-12 w-12 mt-4 mb-4"/>
-                <img src="public/svg/Jquery.png" className="h-12 w-12 mt-4 mb-4" />
-                <img src="public/svg/Boostrap.png" className="h-12 w-12 mt-4 mb-4"/>
-                <img src="public/svg/Figma.png" className="h-12 w-14"/>
-                <img src="public/svg/Git.png" className="h-13 w-12" />
-                <img src="public/svg/Github.png" className="h-12 w-12" />
-                <img src="public/svg/Tailwind.png"className="h-16 w-16"/>
-                <img src="public/svg/React.png" className="h-12 w-14"/>
-                <img src="public/svg/Rest API.png" className="h-12 w-14"/>
-
+         <div className="columns-12 mt-4 ">
+         <div className="border-skills">
+         <img src="/svg/HTML5.png" className="h-12 w-12  mb-4 "/>
+         </div>
+         <div className="border-skills">
+         <img src="/svg/CSS3.png" className="h-12 w-12 mt-4 mb-4"/>
+         </div>
+         <div className="border-skills">
+         <img src="public/svg/Javascript_logo.png" className="h-12 w-12 mt-4 mb-4"/>
+         </div>
+         <div className="border-skills">
+         <img src="/svg/Jquery.png" className="h-12 w-12 mt-4 mb-4" />
+         </div>
+         <div className="border-skills">
+         <img src="/svg/Boostrap.png" className="h-12 w-12 mt-4 mb-4"/>
+         </div>
+         <div className="border-skills">
+         <img src="/svg/Figma.png" className="h-12 w-14"/>
+         </div>
+         <div className="border-skills">
+         <img src="/svg/Github.png" className="h-12 w-12" />
+         </div>
+         <div className="border-skills">
+         <img src="public/svg/Tailwind.png"className="h-16 w-16"/>
+        </div>
+         <div className="border-skills">
+         <img src="/svg/React.png" className="h-12 w-14"/>
+         </div>
+         <div className="border-skills">
+         <img src="public/svg/Rest API.png" className="h-12 w-14"/>
+         </div>
          </div>
         </>
     );
