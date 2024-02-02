0cd4db48064d53e21158c87c31db5377788e401d
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-02 15:31:00 +0200

    created a bontact button

---
+++ vite-project/src/Components/Contactbutton.jsx
@@ -0,0 +1,13 @@
+import './Contactbutton.css'
+
+const Contactbutton = () => {
+      return (
+        <> 
+        <button type="contact-btn,button me-4 mr-4 mt-4" className="button">
+          <span className="button__text-contact">Get in Touch</span>
+        </button>
+        </>
+      );
+    };
+
+  export default Contactbutton;
