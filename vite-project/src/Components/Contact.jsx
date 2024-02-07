b75b781f0c6959e167d1b9e9f792896b33929d47
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-07 23:40:13 +0200

    added grid layout

--- vite-project/src/Components/Contact.jsx
+++ vite-project/src/Components/Contact.jsx
@@ -1,9 +1,15 @@
 import ContactForm from './ContactForm';
 
 const Contact = () => {
-    return (
-    <div>
+    return (   
+    <div className="grid grid-cols-4 gap-0 ">
+    <div className="col-span-2 col-start-1 col-end-4 ">
     <ContactForm />
+    </div>
+    <div className='illustration grid-span-3'>
+        <h1 className="text-3xl font-bold text-gray-800">Contact Us</h1>
+        <img src="./public/images/illustration.png" />
+    </div>
     </div>
     );
 };
