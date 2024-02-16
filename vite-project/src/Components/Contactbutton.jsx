f62501b42f0847041de3d43d441672c9097b6ec1
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-16 23:56:46 +0200

    rename dthe button to Hire me

--- vite-project/src/Components/Contactbutton.jsx
+++ vite-project/src/Components/Contactbutton.jsx
@@ -4,7 +4,7 @@ const Contactbutton = () => {
       return (
         <> 
         <button type="contact-btn,button me-4 mr-4 mt-4" className="button">
-          <span className="button__text-contact">Get in Touch</span>
+          <span className="button__text-contact">Hire Me</span>
         </button>
         </>
       );
