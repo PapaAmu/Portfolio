c3f690b8c00f2763c213bbfbee356fd932ad2010
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-16 23:55:18 +0200

    removed about imports and appLayout

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,21 @@
 import './App.css'
 import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider,Outlet } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import Navbar from './Components/Navbar'
 import Home from './Components/Home';
-import About from './Components/About';
 import TechnicalSkills from './Components/TechnicalSkills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
-import ContactForm from './Components/ContactForm';
 import Footer from './Components/Footer';
 
-const AppLayout = () => (
-  <>
-    <Navbar />
-    <Outlet />
-  </>
-);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<AppLayout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="technicalskills" element={<TechnicalSkills />} />
-      <Route path="projects" element={<Projects />} />
-      <Route path="contactForm" element={<ContactForm />} />
-      <Route path="contact" element={<Contact />} />
+    <Route element={<Navbar />}>
+     <Route path="/" element={<Home />} />
+        <Route path="/TechnicalSkills" element={TechnicalSkills} />
+        <Route path="/Projects" element={Projects} />
+        <Route path="/Contact" element={Contact} />
     </Route>
   )
 )
@@ -34,7 +24,6 @@ function App() {
   return (
     <>
       <Home />
-      <About />
       <TechnicalSkills />
       <Projects />
       <Contact />
