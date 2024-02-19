acbe417e3663043ca1f8ad03378b535203718104
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-19 23:14:46 +0200

    ceated blog file with post links and images

---
+++ vite-project/src/Components/Blog.jsx
@@ -0,0 +1,48 @@
+
+
+const Blog = () => {
+    return (
+    <>
+    <div className="container-fluid bg-black text-white py-5">
+    <h1>Blog_Post</h1>
+    <p className="blog-paragraph">As a Technical Writer with two years of experience, I bring a unique combination of skills to the table, a background in frontend development and a passion for leading and nurturing a community of fellow technical writers.
+       My journey in this field has equipped me with the ability to craft clear and comprehensive technical articles.
+    </p>
+    <div className="container text-center">
+    <div className="row align-items-center">
+    <div className="col">
+    <div className="card" style="width: 18rem;">
+    <div className="card-body">
+    <img src="https://miro.medium.com/v2/resize:fit:828/format:webp/1*bk_5gREozsja4xXsO-jhPA.jpeg" className="card-img-top" alt="..." />
+    <p className="card-text">The Battle of Browsers: Custom CSS vs. Frameworks</p>
+    <a href="#https://hlobisilelukhele.medium.com/the-battle-of-browsers-custom-css-vs-frameworks-fd3d65e1aab8" className="btn btn-primary">Article</a>
+  </div>
+</div>
+    </div>
+    <div className="col">
+    <div className="card" style="width: 18rem;">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+    <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*B1rK42KjrwD51TnzIYkTOw.png" className="card-img-top" alt="..." />
+    <p className="card-text">Enhancing Web Forms: Data Validation with HTML5 and JavaScript</p>
+    <a href="#https://hlobisilelukhele.medium.com/enhancing-web-forms-data-validation-with-html5-and-javascript-5a3660ee1999" className="btn btn-primary">Read article</a>
+   </div>
+</div>
+    </div>
+    <div className="col">
+    <div className="card" style="width: 18rem;">
+    <div className="card-body">
+    <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*RvjW8NXdrTIJRXK7V-a5hQ.png" className="card-img-top" alt="..." />
+    <p className="card-text">CSS Variables: How and When to use them?</p>
+    <a href="#https://hlobisilelukhele.medium.com/css-variables-how-and-when-to-use-them-3a4ca51e0d22" className="btn btn-primary">Read article</a>
+  </div>
+</div>
+    </div>
+    </div>
+    </div>  
+    </div>
+    </>
+    )
+}
+
+export default Blog;
