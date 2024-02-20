229c011c2aaa3c69d91351a2d7566032a67ee429
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-20 23:00:02 +0200

    increased padding top to 6

--- vite-project/src/Components/Contactbutton.jsx
+++ vite-project/src/Components/Contactbutton.jsx
@@ -3,7 +3,7 @@ import './Contactbutton.css'
 const Contactbutton = () => {
       return (
         <> 
-        <button type="contact-btn,button me-4 mr-4 mt-4" className="button">
+        <button type="contact-btn,button me-4 mr-4 mt-4 pt-6" className="button">
           <span className="btn btn-primary">Hire Me</span>
         </button>
         </>
