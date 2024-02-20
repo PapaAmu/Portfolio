765d2a70f0a10ebb61717c62523f9ac7f8dcd100
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-20 22:57:06 +0200

    created column with skills card using svg icons and styled them

--- vite-project/src/Components/TechnicalSkills.jsx
+++ vite-project/src/Components/TechnicalSkills.jsx
@@ -1,45 +1,84 @@
-import {Typography} from "@material-tailwind/react";
+import './Contactbutton.css'
 
 const TechnicalSkills = () => {
     return (
-        <>
-        <Typography variant="h4" color="blue-gray" className="text-center font-serif mb-4 mt-4">
-           Technical Skills
-          </Typography>
-         
-         <div className="columns-12 mt-4 ">
-         <div className="border-skills">
-         <img src="/svg/HTML5.png" className="h-12 w-12  mb-4 "/>
-         </div>
-         <div className="border-skills">
-         <img src="/svg/CSS3.png" className="h-12 w-12 mt-4 mb-4"/>
-         </div>
-         <div className="border-skills">
-         <img src="public/svg/Javascript_logo.png" className="h-12 w-12 mt-4 mb-4"/>
-         </div>
-         <div className="border-skills">
-         <img src="/svg/Jquery.png" className="h-12 w-12 mt-4 mb-4" />
-         </div>
-         <div className="border-skills">
-         <img src="/svg/Boostrap.png" className="h-12 w-12 mt-4 mb-4"/>
-         </div>
-         <div className="border-skills">
-         <img src="/svg/Figma.png" className="h-12 w-14"/>
-         </div>
-         <div className="border-skills">
-         <img src="/svg/Github.png" className="h-12 w-12" />
-         </div>
-         <div className="border-skills">
-         <img src="public/svg/Tailwind.png"className="h-16 w-16"/>
-        </div>
-         <div className="border-skills">
-         <img src="/svg/React.png" className="h-12 w-14"/>
-         </div>
-         <div className="border-skills">
-         <img src="public/svg/Rest API.png" className="h-12 w-14"/>
-         </div>
-         </div>
-        </>
+  <div className="container text-center">
+
+    <div className="container-fluid">
+   <div className="row sm:col-sm-2">
+    <div className="col-sm-4 mt-4 mb-4 ">
+    <h2 className="mt-8 pt-12  mb-4 text-center text-bold">Tech_Stack<span className="name-tag">............</span></h2>
+    <h4>Let us explore some of my <span className="name-tag">skills</span></h4>
+    </div>
+    <div className="col-sm-8 ">
+    <div className="row row-cols-3 row-cols-md-5 mt-4 mb-4">
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center border-r-4 bg-color-black">
+    <img src="./svg/HTML5.png" alt="HTML5 logo" className="pt-2 pl-2 pe-2 pb-1"  />
+    <h5 className="card-titl mt-2 text-bold">HTML</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/CSS3.png" alt="CSS logo" className="pt-2 pl-2 pe-2"   />
+    <h5 className="card-titl mt-2 text-bold">CSS</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/JavaScript_logo.png" alt="Javascript logo" className="pt-2 pl-2 pe-2"  />
+    <h5 className="card-titl mt-2 text-bold">Javascript</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/React.png" alt="React logo" className="pt-3 pl-2 pe-2 " />
+    <h5 className="card-titl mt-2 text-bold">React</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/Redux.png" alt="React logo" className="pt-2 pl-2 pe-2" />
+    <h5 className="card-titl mt-2 text-bold">Redux</h5>
+    </div>
+    </div>
+  </div>
+  <div className="row row-cols-3 row-cols-md-5 mt-4 mb-4">
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/Git.png" alt="Your image description" className="pt-2 pl-2 pe-2"  />
+    <h5 className="card-titl mt-2 text-bold">Git</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/tailwind.png" alt="Redux logo" className="pt-2 pl-2 pe-2"  />
+    <h5 className="card-titl mt-2 text-bold">tailwind</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/Firebase.png" alt="Tailwind css logo" className="pt-2 pl-2 pe-2"  />
+    <h5 className="card-titl mt-2 text-bold">Firebase</h5>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/framer-motion.svg" alt="Git logo" className="pt-2 pl-2 pe-2"  />
+    <p className="card-titl mt-2 text-bold">Framer Motion</p>
+    </div>
+    </div>
+    <div className="col-sm mt-4 mb-4">
+    <div className="card items-center">
+    <img src="./svg/Mysql.png" alt="Github logo" className="pt-2 pl-2 pe-2"/>
+    <h5 className="card-titl mt-2 text-bold">Mysql</h5>
+    </div>
+    </div>
+  </div>
+    </div>
+  </div>
+  </div>
+  </div>
     );
 };
 
