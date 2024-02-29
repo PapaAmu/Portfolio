764a3b9d7554c1f305d5c908c4e6ab366e3d7910
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-02-29 23:48:25 +0200

    animated image and  to zoom-in

--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
        <p className="text-slate-200 text-black text-left pt-8 mb-3 ml-12 mr-12 pb-4 text-md">Frontend developer that love to convert <b>designs</b> to <b>code</b>. I have passion for breathing life into pixels and  making my clients happy one interface at a time.When I am not coding, I am a newsletter contribution  tech newsletter, transforming complex technical concepts to clear and concise content through technical writing.</p>
        <p className="text-slate-200 text-black text-left pb-2  ml-12 mr-12 text-md">So if you are looking for someone who not only builds engaging and interacive user interfaces but also excels at translating complex tech jargons into clear accessible language, then I am your girl 😉.</p>
        <div className=" sm:mt-4 space-x-4 sm:flex text-left ml-12 mr-12 pr-15 pt-3 pb-9 ">
+       <div data-aos="zoom-in">
        <div className='col-sm6 d-flex align-items-center'> 
        <Resumebutton className=" pt-8 sm:mt-4 mb-4 pb-8">
         <a href="Hlobisile Lukhele resume (10).pdf" target="_blank" download="HlobisileLukhele Resume"/>
@@ -18,9 +19,13 @@ function Home() {
         <Contactbutton />
        </div>
        </div>
+       </div>
     </div>
+    
     <div className="col-sm- sm:justify-items-center mt-12 pt-8 ">
+    <div data-aos="zoom-in"> 
       <img className="Head-shot md:h-65 md:w-70 mb-4, lg:h-65 md:w-60 mb-4, sm:h-65 w-60 text-center mt-10 object-center rounded"src="src/images/head-shot.png" alt="Head-shot image" />
+      </div>
     </div>
     </div>
     </div>
