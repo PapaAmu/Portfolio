60f358baca54f0bd6f2e4f15d8cff5ed95f82819
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-03-03 19:25:36 +0200

    reduced margin right

--- vite-project/src/Components/Contactbutton.jsx
+++ vite-project/src/Components/Contactbutton.jsx
@@ -3,8 +3,8 @@ import './Contactbutton.css'
 const Contactbutton = () => {
       return (
         <> 
-        <button type="contact-btn,button me-4 mr-4 mt-4 pt-6" className="button">
-          <span className="btn btn-primary">Hire Me</span>
+        <button type="contact-btn,button  mr-2 mt-4 pt-6" className="button">
+          <span className="btn btn-primary">Contact</span>
         </button>
         </>
       );
