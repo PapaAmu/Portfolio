fb3be4e49b86882ef0dbc47a2088d62fb86dda78
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-03-03 20:25:15 +0200

    modified image folders

--- vite-project/src/Components/TechnicalSkills.jsx
+++ vite-project/src/Components/TechnicalSkills.jsx
@@ -19,37 +19,37 @@ const TechnicalSkills = () => {
     <div className="row row-cols-3 row-cols-md-6 mt-4 mb-4">
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center border-r-4 bg-color-black">
-    <img src="./src/svg/HTML5.png" alt="HTML5 logo" className="pt-2 pl-2 pe-2 pb-1"  />
+    <img src="./assets/svg/HTML5.png" alt="HTML5 logo" className="pt-2 pl-2 pe-2 pb-1"  />
     <h5 className="card-titl mt-2 text-bold">HTML</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/CSS3.png" alt="CSS logo" className="pt-2 pl-2 pe-2"   />
+    <img src="./public/assets/svg/CSS3.png" alt="CSS logo" className="pt-2 pl-2 pe-2"   />
     <h5 className="card-titl mt-2 text-bold">CSS</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/JavaScript_logo.png" alt="Javascript logo" className="pt-2 pl-2 pe-2"  />
+    <img src="./public/assets/svg/JavaScript_logo.png" alt="Javascript logo" className="pt-2 pl-2 pe-2"  />
     <h5 className="card-titl mt-2 text-bold">Javascript</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Boostrap.png" alt="CSS logo" className="pt-2 pb-2 pl-2 pe-2"   />
+    <img src="./public/assets/svg/Boostrap.png" alt="CSS logo" className="pt-2 pb-2 pl-2 pe-2"   />
     <h5 className="card-titl mt-2 text-bold">Boostrap</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/React.png" alt="React logo" className="pt-3 pl-2 pe-2 " />
+    <img src="./public/assets/svg/React.png" alt="React logo" className="pt-3 pl-2 pe-2 " />
     <h5 className="card-titl mt-2 text-bold">React</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Redux.png" alt="React logo" className="pt-2 pl-2 pe-2" />
+    <img src="./public/assets/svg/Redux.png" alt="React logo" className="pt-2 pl-2 pe-2" />
     <h5 className="card-titl mt-2 text-bold">Redux</h5>
     </div>
     </div>
@@ -60,38 +60,38 @@ const TechnicalSkills = () => {
   <div className="row row-cols-3 row-cols-md-6 mt-4 mb-4">
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Git.png" alt="Your image description" className="pt-2 pl-2 pe-2"  />
+    <img src="./public/assets/svg/Git.png" alt="Your image description" className="pt-2 pl-2 pe-2"  />
     <h5 className="card-titl mt-2 text-bold">Git</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Github.png" alt="CSS logo" className="pt-2 pl-2 pe-2"   />
+    <img src="./public/assets/svg/Github.png" alt="CSS logo" className="pt-2 pl-2 pe-2"   />
     <h5 className="card-titl mt-2 text-bold">Github</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Firebase.png" alt="Tailwind css logo" className="pt-2 pl-2 pe-2"  />
+    <img src="./public/assets/svg/Firebase.png" alt="Tailwind css logo" className="pt-2 pl-2 pe-2"  />
     <h5 className="card-titl mt-2 text-bold">Firebase</h5>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/tailwind.png" alt="Redux logo" className="pt-2 pl-2 pe-2"  />
+    <img src="./public/assets/svg/tailwind.png" alt="Redux logo" className="pt-2 pl-2 pe-2"  />
     <h5 className="card-titl mt-2 text-bold">tailwind</h5>
     </div>
     </div>
     
     <div className="col-sm mt-4 mb-4 .d-sm-none">
     <div className="card items-center">
-    <img src="./src/svg/framer-motion.svg" alt="Git logo" className="pt-1 pb-0 pl-2 pe-2"  />
+    <img src="./public/assets/svg/framer-motion.svg" alt="Git logo" className="pt-1 pb-0 pl-2 pe-2"  />
     <p className="card-titl mt-2 text-bold text-sm">Framer </p>
     </div>
     </div>
     <div className="col-sm mt-4 mb-4">
     <div className="card items-center">
-    <img src="./src/svg/Mysql.png" alt="Github logo" className="pt-2 pl-2 pe-2"/>
+    <img src="./public/assets/svg/Mysql.png" alt="Github logo" className="pt-2 pl-2 pe-2"/>
     <h5 className="card-titl mt-2 text-bold">Mysql</h5>
     </div>
     </div>
