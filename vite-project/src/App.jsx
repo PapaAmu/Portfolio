0121542eb5772e3797d381a93ba0dcc0423e2292
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-03-03 19:10:41 +0200

    removed react-router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,27 +3,12 @@ import './App.css'
 import './index.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider,Router,Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar'
 import Home from './Components/Home';
 import TechnicalSkills from './Components/TechnicalSkills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Router> 
-    <Navbar />
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/TechnicalSkills" element={TechnicalSkills} />
-        <Route path="/Projects" element={Projects} />
-        <Route path="/Contact" element={Contact} />
-    </Routes>
-    </Router>
-  )
-)
 
 function App() {
   useEffect(() => {
@@ -43,9 +28,5 @@ function App() {
     </>
   );
 }
-
-<RouterProvider
-    router={router}
-  />
 
 export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,9 +6,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //<React.StrictMode>
   <BrowserRouter>
       <App />
   </BrowserRouter>
-  </React.StrictMode>,
+  //</React.StrictMode>,
 )
