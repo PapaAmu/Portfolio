8d64540d283479f40ba107e83b32dd4e43c40bbc
Hlobisile Lukhele <n.thwala03@gmail.com>
2024-03-14 16:17:05 +0200

    changed the message

--- vite-project/src/Components/Contact.jsx
+++ vite-project/src/Components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     <div className="col-sm-6 pt-20  pl-5 text-center">
       <h1 className="heading-text tracking-wide pt-16">Hire Me Today</h1>
       <p className="hook-up-line">Let us bring your products to life</p>
-      <button className="btn btn-secondary message mb-4 mt-4 "> Shoot me a Message 👉</button>
+      <button className="btn btn-secondary message mb-4 mt-4 "> Reach out here </button>
     </div>
     <div className="col-sm-6 text-left pr-4 pt-18">
     <ContactForm />
